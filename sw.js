269116b5d81318b36615ebe4409b49a2d22de2c7
CK <ck@local>
2026-04-29 17:22:15 +0800

    Separate assigned admin leads from progress sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.04.29.0012';
+const APP_VERSION = '2026.04.29.0013';
 const CACHE_NAME = `autodial-v2-${APP_VERSION}`;
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
