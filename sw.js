8f547facb6a240779e0e13a275e6751a6d522a10
FundLens-SG <271685815+FundLens-SG@users.noreply.github.com>
2026-05-01 03:05:37 +0800

    Highlight preferred appointment durations in slots

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.05.01.0002';
+const APP_VERSION = '2026.05.01.0003';
 const CACHE_NAME = `autodial-v2-${APP_VERSION}`;
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
