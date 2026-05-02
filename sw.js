11cf9022ffe43dc605f5df1b6c1c9a7f72cd042c
FundLens-SG <271685815+FundLens-SG@users.noreply.github.com>
2026-05-02 20:00:28 +0800

    Harden header controls and callback lookup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.05.02.0010';
+const APP_VERSION = '2026.05.02.0011';
 const CACHE_NAME = `autodial-v2-${APP_VERSION}`;
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
